01664043e32a57810dba1e22b74802e7f63d5d8d
Angeles Guyot <angeles.guyot@kellogg.northwestern.edu>
2024-05-01 23:28:01 +0000

    save

--- kmdb.rb
+++ kmdb.rb
@@ -75,14 +75,46 @@
 
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
-# TODO!
+Movie.destroy_all
+Studio.destroy_all
+Actor.destroy_all
+Role.destroy_all
 
 # Generate models and tables, according to the domain model.
-# TODO!
+
+    # rails generate model actor, studio, movie, role
+    # add columns according to hw1
+    # rails db:migrate
 
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
-# TODO!
+
+new_studio = Studio.new
+new_studio ["name"] = "Warner Bros."
+new_studio.save
+
+warner_bros = Studio.find_by({"name"=>"Warner Bros."})
+
+new_movie = Movie.new
+new_movie["title"] = "Batman Begins"
+new_movie["year_released"] = 2005
+new_movie["rated"] = "PG-13"
+new_movie["studio_id"] = warner_bros.id
+new_movie.save
+
+new_movie = Movie.new
+new_movie["title"] = "The Dark Knight"
+new_movie["year_released"] = 2008
+new_movie["rated"] = "PG-13"
+new_movie["studio_id"] = warner_bros.id
+new_movie.save
+
+new_movie = Movie.new
+new_movie["title"] = "The Dark Knight Rises"
+new_movie["year_released"] = 2012
+new_movie["rated"] = "PG-13"
+new_movie["studio_id"] = warner_bros.id
+new_movie.save
 
 # Prints a header for the movies output
 puts "Movies"
